7a86b58455d2eab13c977d36654310382ad39452
Maria Gamzina <gamzinamaria@gmail.com>
2024-03-29 23:23:42 +0300

    change

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,8 +1,8 @@
 public class BonusMilesService {
 
     public int calculate(int price) {
-       int result;
-       result = price/20;
-       return result;
+        int result;
+        result = price / 20;
+        return result;
     }
 }
